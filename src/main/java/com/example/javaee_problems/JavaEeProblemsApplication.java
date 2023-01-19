86954aaca9e5f9e4eec6dc2a353a23a7e58a7f2f
FraserBogs <aliyevzakir814@gmail.com>
2023-01-19 07:05:32 +0400

    Java EE Problems

---
+++ src/main/java/com/example/javaee_problems/JavaEeProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaee_problems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeProblemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEeProblemsApplication.class, args);
+    }
+
+}
